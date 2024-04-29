6d7dd2bafb4ee902852b566efbc1c7d1cfc36c2b
adyasha15 <madyasha460@gmail.com>
2024-04-29 12:31:48 +0530

    2nd commit

---
+++ src/main/java/com/airbnb/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.airbnb.controller;
+
+public class PaymentController {
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
